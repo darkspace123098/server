bf7e3e8ecb3dfb29e5aa78bea70f39ef22b9c86c
Dark Space <150314290+darkspace123098@users.noreply.github.com>
2024-02-21 14:39:09 +0000

    sdfgn

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express'
+
+const app = express();
+require('dotenv').config();
+
+// const port = 4000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/about', (req, res) => {
+    const data = "<h1>Welcome</h1><br><input>"
+    res.send(data)
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
 })
